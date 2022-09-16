d2136a578f84835a3cd5ada1adda3a56a4e70b68
wangshuai <shuaiwang2019@126.com>
2022-09-16 10:06:25 +0800

    master  commit 2

--- src/com/ws/hello/TestHello.java
+++ src/com/ws/hello/TestHello.java
@@ -6,5 +6,6 @@ public class TestHello {
         System.out.println("Hello World!2");
         System.out.println("Hello World!3");
         System.out.println("hot-fix test");
+        System.out.println("master test");
     }
 }
