e566af79125eb4e83c8f96f4d9535c9c40a1a083
wangshuai <shuaiwang2019@126.com>
2022-09-16 09:54:53 +0800

    third commit

--- src/com/ws/hello/TestHello.java
+++ src/com/ws/hello/TestHello.java
@@ -2,7 +2,8 @@ package com.ws.hello;
 
 public class TestHello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println("Hello World!1");
+        System.out.println("Hello World!2");
+        System.out.println("Hello World!3");
     }
 }
